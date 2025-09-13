e00f2428a0bbac2d47adf93ffc0dbf1a8c37b2ce
Dhruvil <dhruvilgandhi1203@gmail.com>
2025-09-13 21:32:56 +0530

    created class version of cart

--- Scripts/checkout.js
+++ Scripts/checkout.js
@@ -1,6 +1,6 @@
 import { renderOrderSummary } from "./Checkout/orderSummary.js";
 import { renderPaymentSummary } from "./Checkout/paymentSummary.js";
-import "../data/cart_OOP.js"
+import "../data/cart_class.js"
 
 renderOrderSummary();
 renderPaymentSummary();
---
+++ data/cart_class.js
@@ -0,0 +1,90 @@
+class Cart {
+  cartItems;
+  localStorageKey;
+
+  constructor(localStorageKey) {
+    this.localStorageKey = localStorageKey;
+    this.loadFromStorage();
+  }
+
+  loadFromStorage () {
+    this.cartItems = JSON.parse (localStorage.getItem (this.localStorageKey));
+
+    if (!this.cartItems) {
+        this.cartItems = [
+        {
+            productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+            quantity: 2,
+            deliveryOptionId: '1' 
+        },
+        {
+            productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+            quantity: 3,
+            deliveryOptionId: '2'
+        }
+        ];
+    }
+  }
+
+  saveToStorage() {
+    localStorage.setItem(this.localStorageKey, JSON.stringify(this.cartItems));
+  }
+
+  addToCart(productId, quantity = 1) {
+      let item = this.cartItems.find(p => p.productId === productId);
+      if (item) {
+          item.quantity += quantity;
+      } else {
+          this.cartItems.push({ 
+              productId, 
+              quantity: quantity,
+              deliveryOptionId: '1'
+          });
+      }
+
+      this.saveToStorage();
+  } 
+
+  // Optional: expose function to get full cart if needed elsewhere
+  getCart() {
+      return this.cartItems;
+  }
+
+  removeFromCart (productId) {
+      const newCart = [];
+
+      this.cartItems.forEach ((cartItem) => {
+          if (cartItem.productId != productId) {
+              newCart.push (cartItem);
+          }
+      });
+
+      this.cartItems = newCart;
+
+      this.saveToStorage();
+  }
+
+  updateDeliveryOption(productId, deliveryOptionId) {
+      let matchingItem  = this.cartItems.find(p => p.productId === productId);
+      if (!matchingItem) {
+          console.error(`No cart item found with productId: ${productId}`);
+          return;  // Prevent further error
+      }
+      matchingItem.deliveryOptionId = deliveryOptionId;
+
+      this.saveToStorage();
+  }
+}
+
+
+const cart = new Cart("cart_OOP");
+const businessCart = new Cart("cart_business");
+
+
+
+console.log (cart);
+console.log (businessCart);
+console.log (businessCart instanceof Cart);
+
+
+
