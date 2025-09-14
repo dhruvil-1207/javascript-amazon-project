c7f90e89c4f561decaa41f28f670f3a371a341a7
Dhruvil <dhruvilgandhi1203@gmail.com>
2025-09-15 00:54:49 +0530

    learned promises and converted code to promises

--- Scripts/checkout.js
+++ Scripts/checkout.js
@@ -1,11 +1,59 @@
 import { renderOrderSummary } from "./Checkout/orderSummary.js";
 import { renderPaymentSummary } from "./Checkout/paymentSummary.js";
 import { loadProducts } from "../data/products.js";
+import {loadCart} from "../data/cart.js";
 // import "../data/cart_class.js"
 // import "../data/backend_practice.js";
 
-loadProducts(() => {
+
+Promise.all([
+  new Promise ((resolve) => {
+    // console.log ("first step");
+    loadProducts(() => {
+      // console.log ("second step");
+      resolve("value1"); 
+    });
+  }),
+
+  new Promise ((resolve) => {
+    loadCart(() => {
+      resolve();
+    });
+  })
+
+]).then((values) => {
+  // console.log (values);
   renderOrderSummary();
   renderPaymentSummary();
 });
+
+
+
+// new Promise ((resolve) => {
+//   // console.log ("first step");
+//   loadProducts(() => {
+//     // console.log ("second step");
+//     resolve("value1"); 
+//   })
+
+// }).then ((value) => {
+//   // console.log ("next step");
+//   console.log (value);
+//   return new Promise ((resolve) => {
+//     loadCart(() => {
+//       resolve();
+//     });
+//   });
+
+// }).then(() => {
+//   renderOrderSummary();
+//   renderPaymentSummary();
+//   });
+
+// loadProducts(() => {
+//   loadCart(() => {
+//     renderOrderSummary();
+//     renderPaymentSummary();
+//   });
+// });
 
